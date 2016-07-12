a193db41a5bc55d211757aaa25d77809f66d0261
Junqiang Qian <jq514@ic.ac.uk>
2016-07-12 23:52:16 +0100

    testing ssh

--- src/02_03.cpp
+++ src/02_03.cpp
@@ -49,7 +49,7 @@ vector<int> merge(vector<int> v, vector<int> w) {
 }
 
 int main(int argc, char *argv[]) {
-  vector<int> v = {9, 8, 7, 6, 5, 1, 2, 3, 4, 10};
+  vector<int> v = {9, 8, 7, 6, 5, 1, 2, 3, 4, 10, 11, 12};
   vector<int> res = merge_sort(v);
   for (int i = 0; i < res.size(); i++) {
     cout << res[i] << " ";
