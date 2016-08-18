40b2605002c402401291400d636517df772352ae
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2016-08-18 17:08:43 +0100

    Added Chapter 8: Counting Sort

---
+++ src/08_sorting_in_linear_time.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+void counting_sort(vector<T>& a, vector<T>& b, long long k) {
+  vector<T> c(k + 1);
+  for (unsigned j = 0; j < a.size(); j++) {
+    c[a[j]]++;
+  }
+  for (unsigned i = 1; i <= k; i++) {
+    c[i] += c[i - 1];
+  }
+  for (int i = a.size() - 1; i >= 0; i--) {
+    b[c[a[i]] - 1] = a[i];
+    c[a[i]]--;
+  }
+}
+
+int main() {
+  vector<int> a = {8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  vector<int> b(a.size());
+  counting_sort(a, b, 9);
+  std::copy(b.begin(), b.end(), std::ostream_iterator<int>(std::cout, " "));
+}
