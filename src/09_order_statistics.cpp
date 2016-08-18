101c131f3f17a1d03744e64e4a07f702bc77d082
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2016-08-18 18:56:20 +0100

    Added randomised select function to calculate ith order statistic

--- src/09_order_statistics.cpp
+++ src/09_order_statistics.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <algorithm> /* min(), max() */
+#include <stdlib.h> /* rand() */
+#include <time.h> /* time */
+
+#define SWAP(x, y, T) do { T SWAP = x; x = y; y = SWAP; } while (0)
 
 using namespace std;
 
@@ -76,8 +80,54 @@ Pair<T> minimumAndMaximum(vector<T>& v) {
     return results;
 }
 
+template <typename T>
+int partition(vector<T>& v, int p, int r) {
+    T pivot = v[r];
+    int i = p - 1;
+    for (unsigned j = p; j < r; j++) {
+        if (v[j] <= pivot) {
+            i++;
+            SWAP(v[i], v[j], T);
+        }
+    }
+    SWAP(v[i + 1], v[r], T);
+    return i + 1;
+}
+
+int generateRandomNumberBetween(int p, int r) {
+    srand(time(NULL));
+    int difference = r - p + 1;
+    return p + rand() % difference;
+}
+
+template <typename T>
+int randomised_partition(vector<T>& v, int p, int r) {
+    int randIndex = generateRandomNumberBetween(p, r);
+    SWAP(v[r], v[randIndex], T);
+    return partition(v, p, r);
+}
+
+
+template <typename T>
+T randomised_select(vector<T>& v, int p, int r, int i) {
+    if (p == r) {
+        return v[p];
+    }
+    int q = randomised_partition(v, p, r);
+    int k = q - p + 1;
+    if (i == k) {
+        return v[q];
+    } else if (i < k) {
+        return randomised_select(v, p, q - 1, i);
+    } else {
+        return randomised_select(v, q + 1, r, i - k);
+    }
+}
+
+
 int main() {
-    vector<int> v = {5, 6, 7, 8, 9, 10, -1, 0, 1, 2, 1, 0};
-    Pair<int> results = minimumAndMaximum(v);
-    cout << results.minimum << " " << results.maximum << endl;
+    vector<int> v = {10, 5, 1, 7, 2, 8, 9, 6, 4, 3};
+    int q = randomised_select(v, 0, v.size() - 1, 5);
+    cout << q << endl;
+
 }
